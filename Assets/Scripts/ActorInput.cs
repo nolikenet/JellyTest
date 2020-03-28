6fb89f3a5302af96bbce293c4225bac16df2d8e3
nolike <nolikenet@gmail.com>
2020-03-28 14:10:32 +0200

    initial

---
+++ Assets/Scripts/ActorInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ActorInput : MonoBehaviour {
+
+  private ActorMovement _movement;
+
+  private void Awake() {
+    _movement = GetComponent<ActorMovement>();
+  }
+
+  private void Update() {
+    if (Input.GetKeyDown(KeyCode.W)) {
+      _movement.Rotate(Vector3.right * 90.0f);
+    } else if (Input.GetKeyDown(KeyCode.A)) {
+      _movement.Rotate(Vector3.forward * 90.0f);
+    } else if (Input.GetKeyDown(KeyCode.D)) {
+      _movement.Rotate(Vector3.forward * (-90.0f));
+    } else if (Input.GetKeyDown(KeyCode.S)) {
+      _movement.Rotate(Vector3.right * (-90.0f));
+    }
+  }
+
+}
---
+++ Assets/Scripts/ActorMovement.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class ActorMovement : MonoBehaviour {
+
+  private bool _rotating;
+
+  private void ResetRotation() {
+    transform.rotation = Quaternion.Euler(Vector3.zero);
+    _rotating = false;
+  }
+
+  public void Rotate(Vector3 angleAxis) {
+    if (_rotating) return;
+    _rotating = true;
+    transform.DOLocalRotateQuaternion(transform.localRotation * Quaternion.Euler(angleAxis), 0.5f)
+      .OnComplete(
+        ResetRotation
+      );
+  }
+
+}
